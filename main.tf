22df7b6b4672cb68772504aa978eb1e2220d4211
Harold Adrian <habolanos@e11evenn.co>
2023-06-09 10:40:47 -0500

    add OK EC2 should be in private subnet

--- main.tf
+++ main.tf
@@ -6,11 +6,68 @@
 provider "aws" {
   region = var.region
   profile = "aws-tcs"
+  default_tags {
+    tags = {
+      Environment = "Lab"
+      Scope = "TCS Devops Test"
+    }
+  }
 }
 
-# Set Data Source to get VPS's ID by default
-data "aws_vpc" "default" {
+# Set Data Source to get VPS's ID to Lab
+data "aws_vpc" "lab_vpc" {
   default = true
+}
+
+resource "aws_vpc" "lab_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "lab-vpc"
+  }
+}
+
+resource "aws_subnet" "lab_private_subnet" {
+  vpc_id            = aws_vpc.lab_vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "lab-private-subnet"
+  }
+}
+
+resource "aws_network_interface" "lab_ni" {
+  subnet_id   = aws_subnet.lab_private_subnet.id
+  private_ips = ["10.0.10.100"]
+
+  tags = {
+    Name = "lab-network-interface"
+  }
+}
+
+resource "aws_internet_gateway" "lab_igw" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "lab-internet-gateway"
+  }
+}
+
+resource "aws_eip" "lab_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "lab_nat_gw" {
+  allocation_id = aws_eip.lab_eip.id
+  subnet_id     = aws_subnet.lab_private_subnet.id
+
+  tags = {
+    Name = "lab-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.lab_igw]
 }
 
 # Set EC2's instance with AMI Amazon Linux
@@ -19,8 +76,16 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = var.ec2_type
   vpc_security_group_ids = [aws_security_group.sg_main.id]
 
-  // Escribimos un "here document" que es
-  // usado durante la inicialización
+  network_interface {
+    network_interface_id = aws_network_interface.lab_ni.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  // Make a file to publish it as a page html
   user_data = <<-EOF
               #!/bin/bash
               echo "TSC Devops Test by Harold Adrian." > index.html
@@ -36,7 +101,7 @@ resource "aws_instance" "ec2_instance" {
 # Set security group to access by ports
 resource "aws_security_group" "sg_main" {
   name   = "security-group-main"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.lab_vpc.id
 
   dynamic "ingress" {
     for_each = toset(var.sg_ports.ports_in)
